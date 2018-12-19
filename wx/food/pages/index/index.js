033ea5e2b069ffd8cc3409be2d1576de31c13b8d
离鹤 <44833869+panlvdengxiaoxianbaba@users.noreply.github.com>
2018-12-19 20:17:23 +0800

    Add files via upload

--- wx/food/pages/index/index.js
+++ wx/food/pages/index/index.js
@@ -3,7 +3,18 @@
 const app = getApp()
 
 Page({
-
+  articalShow:function(e){
+      var show=e.target.dataset.show;
+      wx.navigateTo({
+        url:"/pages/article/article?show="+show,
+      })
+  },
+  jumpType:function(e){
+    var type=e.target.dataset.type;
+    wx.navigateTo({
+      url:'/pages/type/type?type='+type,
+    })
+  },
   /**
    * 页面的初始数据
    */
@@ -17,15 +28,21 @@ Page({
       { id: 6, img_url: 'http://127.0.0.1:3000/img/iterator5.jpg' },
       ],
       navlist:[
-        { id: 1, img_url: 'http://127.0.0.1:3000/img/type1.png', title: '' },
-        { id: 2, img_url: 'http://127.0.0.1:3000/img/type2.png', title: '' },
-        { id: 3, img_url: 'http://127.0.0.1:3000/img/type3.png', title: '' },
-        { id: 4, img_url: 'http://127.0.0.1:3000/img/type4.png', title: '' },
-        { id: 5, img_url: 'http://127.0.0.1:3000/img/type5.png', title: '' },
-        { id: 6, img_url: 'http://127.0.0.1:3000/img/type6.png', title: '' },
-        { id: 7, img_url: 'http://127.0.0.1:3000/img/type7.png', title: '' },
-        { id: 8, img_url: 'http://127.0.0.1:3000/img/type8.png', title: '' },
-        { id: 9, img_url: 'http://127.0.0.1:3000/img/type9.png', title: '' },
+        { id: 1, img_url: 'http://127.0.0.1:3000/img/type1.png', type: '10' },
+        { id: 2, img_url: 'http://127.0.0.1:3000/img/type2.png', type: '20' },
+        { id: 3, img_url: 'http://127.0.0.1:3000/img/type3.png', type: '30' },
+        { id: 4, img_url: 'http://127.0.0.1:3000/img/type4.png', type: '40' },
+        { id: 5, img_url: 'http://127.0.0.1:3000/img/type5.png', type: '50' },
+        { id: 6, img_url: 'http://127.0.0.1:3000/img/type6.png', type: '60' },
+        { id: 7, img_url: 'http://127.0.0.1:3000/img/type7.png', type: '70' },
+        { id: 8, img_url: 'http://127.0.0.1:3000/img/type8.png', type: '80' },
+        { id: 9, img_url: 'http://127.0.0.1:3000/img/type9.png', type: '90' },
+      ],
+      showlist:[
+        { id: 1, img_url: 'http://127.0.0.1:3000/img/show1.jpg',show:101},
+        { id: 2, img_url: 'http://127.0.0.1:3000/img/show2.jpg', show: 102},
+        { id: 3, img_url: 'http://127.0.0.1:3000/img/show3.jpeg', show: 103},
+        { id: 4, img_url: 'http://127.0.0.1:3000/img/show4.jpg', show: 104}
       ]
   },
 
